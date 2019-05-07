3db3ad1037530944cf8964a2986b5bcb15068e0c
clydeu <42592071+clydeu@users.noreply.github.com>
2019-05-07 14:37:08 +1000

    Create RemovePackages.ps1

---
+++ RemovePackages.ps1
@@ -0,0 +1,2 @@
+choco uninstall -y --removedependencies firefox git.install sourcetree visualstudio2019community youtube-dl sqlitebrowser mkdocs slack putty nuget.commandline
+paint.net nugetpackageexplorer notepadplusplus-npppluginmanager notepadplusplus.install dotpeek vscode docker-desktop wsl-ubuntu-1804
